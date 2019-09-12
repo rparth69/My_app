ec1412f849a13ce416c1f9b40eaa172bee3263ed
rparth69 <rparth69@gmail.com>
2019-09-12 12:27:02 +0530

    errorslove

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,11 +9,12 @@ class UsersController < ApplicationController
   end
 
   def create
-    if @user = User.new(user_params)
-      @user.save
+    @user = User.new(user_params)
+    if @user.save
       redirect_to @user
     else
       flash[:danger] = 'Alredy Number Added !'
+      redirect_to root_path
     end
   end
 
