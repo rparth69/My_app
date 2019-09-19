b65cc1a51b6ea75672c0a02e464a8a715e200692
rparth69 <rparth69@gmail.com>
2019-09-19 13:45:20 +0530

    uservaildations

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,6 @@ class User < ApplicationRecord
   has_many :cities
   has_and_belongs_to_many :cities, join_table: 'users_cities'
   enum user_type: { customer: 0, cleaner: 1 }
+  validates :phone_number, presence: true
   validates_uniqueness_of :phone_number, length: { minimum: 10 }
 end
